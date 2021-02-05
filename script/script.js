c2ab4a78507e2d3af9873a624f32beda25fe75e9
Blwan <forever27drunk@gmail.com>
2021-02-06 02:01:53 +0500

    fixedHardLesson04

--- script/script.js
+++ script/script.js
@@ -2,9 +2,7 @@ function newOne(randVar){
     if (typeof(randVar) == 'number'){
         alert('Sike Thats the wrong Number');
     }else{
-        randVar.trim();
-        randVar = randVar.slice(0, 30) + '...';
-        return randVar;
+        return randVar = randVar.trim().slice(0, 30) + '...';
     }
 }
 console.log(newOne('       With their tanks and their bombs and their bombs and their guns       '));
