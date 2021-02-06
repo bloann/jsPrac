c00b4535ea6b997a1dc9eba20bcd5c697f31d1ff
Blwan <forever27drunk@gmail.com>
2021-02-06 20:57:02 +0500

    readyHardLesson05

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
-let arr = ['79', '261', '697', '22', '74', '879', '626'];
+let arr = ['49', '261', '694', '22', '47', '849', '624'];
 
 console.log(arr);
 
 arr.forEach(element => {
-    if(element.search(/(2|7)/)===0) {
+    if(element.search(/(2|4)/)===0) {
         console.log(element);
     }
 });
+
+let n = 100;
+primeNum:
+for (let i = 2; i <= n; i++){
+    for (let j = 2; j < i; j++) {
+        if(i % j == 0){continue primeNum;}
+    }
+    console.log(i);
+    console.log('Делители этого числа: 1 и ' + i);
+}
