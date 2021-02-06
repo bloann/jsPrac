5c9fe56948a89005877ba828e126ea77549de462
Blwan <forever27drunk@gmail.com>
2021-02-06 20:55:35 +0500

    readyLesson05

--- script/script.js
+++ script/script.js
@@ -1,15 +1,9 @@
-let num = 266219; // число
-let str = num.toString(); // преобразуем в строку
-let numLenght = str.length; // вычисляем длинну
-let a = 1; // это еденица для умножения
+let arr = ['79', '261', '697', '22', '74', '879', '626'];
 
-for(let i = 0; i<numLenght; i++) {
-    let x = str[i]; // берем число из массива
-    a = a * x; // умножаем числа
-}
+console.log(arr);
 
-console.log(a); // получаем результат
-
-console.log(a**3); // возодим в степень 3 при помощи оператора **
-
-console.log(String(a**3).slice(0, 2)); // вывод первых двух цифр 
+arr.forEach(element => {
+    if(element.search(/(2|7)/)===0) {
+        console.log(element);
+    }
+});
