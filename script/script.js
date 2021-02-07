50cae33922b384d313083dee1b553b1b520bcb5e
Blwan <forever27drunk@gmail.com>
2021-02-07 18:57:41 +0500

    readyHeardLesson06

--- script/script.js
+++ script/script.js
@@ -1,15 +1,45 @@
-let num = 266219; // число
-let str = num.toString(); // преобразуем в строку
-let numLenght = str.length; // вычисляем длинну
-let a = 1; // это еденица для умножения
+function game(){
+    let i = 10;
+    function mainGameEngine(){
+        let result = confirm('Угадай число от 1 до 100');
+        if (result == true){
+                function gameEngine(){
+                    let randNum = Math.floor(Math.random() * 100) + 1;
+                    let userNum = prompt('Введите число'); // пробел учитывается как ноль и это фича
+                    
+                    if (!isFinite(userNum)){
+                        alert('Введи число!');
+                        return gameEngine();
+                    }
+                
+                    if(userNum == randNum){
+                        alert('Поздравляю, Вы угадали!!!');
 
-for(let i = 0; i<numLenght; i++) {
-    let x = str[i]; // берем число из массива
-    a = a * x; // умножаем числа
+                    } else if(userNum < randNum){
+                        i--;
+                        alert('Загаданное число больше, осталось попыток ' + i);
+                    } else if(userNum > randNum){
+                        i--;
+                        alert('Загаданное число меньше, осталось попыток ' + i);
+                    }
+                    if(i > 0){
+                        mainGameEngine();
+                    } else if(i <= 0){
+                        result2 = confirm('Попытки закончились, хотите сыграть еще?');
+                        if(result2 == true){
+                            game();
+                        }else if (result2 == false){
+                            alert('Игра окончена');
+                            result = false;
+                        }
+                    }
+                    
+                }
+                gameEngine();
+            } else if (result == false){
+                alert('Игра окончена');
+        }
+    }
+    mainGameEngine();
 }
-
-console.log(a); // получаем результат
-
-console.log(a**3); // возодим в степень 3 при помощи оператора **
-
-console.log(String(a**3).slice(0, 2)); // вывод первых двух цифр 
+game();
