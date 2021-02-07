55d16cbd1985429f8416ce1d5be58a01a0717b66
Blwan <forever27drunk@gmail.com>
2021-02-07 19:07:21 +0500

    fixedHardLesson06

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 function game(){
     let i = 10;
+    let randNum = Math.floor(Math.random() * 100) + 1;
+    console.log(randNum);
     function mainGameEngine(){
         let result = confirm('Угадай число от 1 до 100');
         if (result == true){
                 function gameEngine(){
-                    let randNum = Math.floor(Math.random() * 100) + 1;
                     let userNum = prompt('Введите число'); // пробел учитывается как ноль и это фича
                     
                     if (!isFinite(userNum)){
@@ -18,20 +19,35 @@ function game(){
                     } else if(userNum < randNum){
                         i--;
                         alert('Загаданное число больше, осталось попыток ' + i);
+
+                        if(i > 0){
+                            mainGameEngine();
+                        } else if(i <= 0){
+                            result2 = confirm('Попытки закончились, хотите сыграть еще?');
+                            if(result2 == true){
+                                game();
+                            }else if (result2 == false){
+                                alert('Игра окончена');
+                                result = false;
+                            }
+                        }
+                        
                     } else if(userNum > randNum){
                         i--;
                         alert('Загаданное число меньше, осталось попыток ' + i);
-                    }
-                    if(i > 0){
-                        mainGameEngine();
-                    } else if(i <= 0){
-                        result2 = confirm('Попытки закончились, хотите сыграть еще?');
-                        if(result2 == true){
-                            game();
-                        }else if (result2 == false){
-                            alert('Игра окончена');
-                            result = false;
+
+                        if(i > 0){
+                            mainGameEngine();
+                        } else if(i <= 0){
+                            result2 = confirm('Попытки закончились, хотите сыграть еще?');
+                            if(result2 == true){
+                                game();
+                            }else if (result2 == false){
+                                alert('Игра окончена');
+                                result = false;
+                            }
                         }
+
                     }
                     
                 }
