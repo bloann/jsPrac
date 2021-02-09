7a1cab6bfd2c1b74e7318f8c33cbca33fd77f68c
Blwan <forever27drunk@gmail.com>
2021-02-09 13:41:16 +0500

    fixedLesson06

--- script/script.js
+++ script/script.js
@@ -1,25 +1,34 @@
 function game(){
     let result = confirm('Угадай число от 1 до 100');
-    if (result == true){
-            function gameEngine(){
+    function checkResult(){
+        if (result == true){
                 let randNum = Math.floor(Math.random() * 100) + 1;
-                let userNum = prompt('Введите число'); // пробел учитывается как ноль и это фича
-                
-                if (!isFinite(userNum)){
-                    alert('Введи число!');
-                    return gameEngine();
-                }
-            
-                if(userNum == randNum){
-                    alert('Поздравляю, Вы угадали!!!');
-                } else if(userNum < randNum){
-                    alert('Загаданное число больше');
-                } else if(userNum > randNum){
-                    alert('Загаданное число меньше');
-                }
-                game();
-            }
-            gameEngine();
+                console.log(randNum);
+                    function gameEngine(){
+                        let userNum = prompt('Введите число');
+                        if (!isFinite(userNum)){
+                            alert('Введи число!');
+                            return gameEngine();
+                        }
+                    
+                        if(userNum == randNum){
+                            alert('Поздравляю, Вы угадали!!!');
+                            checkResult();
+                        }else if(userNum == null){
+                            result = false;
+                            checkResult();
+                        } else if(userNum < randNum){
+                            alert('Загаданное число больше');
+                            gameEngine();
+                        } else if(userNum > randNum){
+                            alert('Загаданное число меньше');
+                            gameEngine();
+                        }
+                    }
+                    gameEngine();
         } else if (result == false){ alert('Игра окончена'); }
+    }
+    checkResult();
+    
 }
 game();
