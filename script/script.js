8127791313b56a9eebf0586ea78819c78c812daa
Blwan <forever27drunk@gmail.com>
2021-02-18 05:09:19 +0500

    ready

--- script/script.js
+++ script/script.js
@@ -1,15 +1,69 @@
-let num = 266219; // число
-let str = num.toString(); // преобразуем в строку
-let numLenght = str.length; // вычисляем длинну
-let a = 1; // это еденица для умножения
+'use strict';
 
-for(let i = 0; i<numLenght; i++) {
-    let x = str[i]; // берем число из массива
-    a = a * x; // умножаем числа
-}
+const DomElement = function (selector, height, width, bg, fontSize ){
+    const body = document.querySelector('body');
 
-console.log(a); // получаем результат
+    DomElement.prototype.rectBlock = function (){
+        let top = 0,
+        left = 0;
 
-console.log(a**3); // возодим в степень 3 при помощи оператора **
+        const div = document.createElement('div');
+        
+        body.append(div);
+        
+        div.style.cssText = `height: 100px; width: 100px; background: red; position: absolute;`;
+        
 
-console.log(String(a**3).slice(0, 2)); // вывод первых двух цифр 
+        const func = function (event){
+            switch (event.key) {
+                case `ArrowUp`:
+                    top -= 10;
+                    div.style.top = `${top}px`;
+                    break;
+                case `ArrowDown`:
+                    top += 10;
+                    div.style.top = `${top}px`;
+                    break;
+                case `ArrowRight`:
+                    left += 10;
+                    div.style.left = `${left}px`;
+                    break;
+                case `ArrowLeft`:
+                    left -= 10;
+                    div.style.left = `${left}px`;
+                    break;
+            }
+        };
+        
+        document.addEventListener('keydown', func);
+    };
+
+    DomElement.prototype.newBlock = function (){
+        if(selector.charAt(0) === '.'){
+            //создаем элемент 
+            const div = document.createElement('div');
+            body.append(div);
+            //задаем класс элементу
+            div.classList.add(selector.slice(1));
+            // задаем стиль
+            div.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize}; `;
+            //Записываем любой текст
+            div.innerHTML = 'Любой текст в div';
+        }else if(selector.charAt(0) === '#'){
+            //создаем элемент 
+            const p = document.createElement('p');
+            body.append(p);
+            //задаем класс элементу
+            p.setAttribute('id', selector.slice(1));
+            // задаем стиль
+            p.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize};`;
+            //Записываем любой текст
+            p.innerHTML = 'Любой текст в p';
+        }
+    };
+};
+
+const newObj = new DomElement(`#block`, `250px`, `100%`, `green`, `100px`);
+
+document.addEventListener('DOMContentLoaded', newObj.rectBlock());
+newObj.newBlock();
