a435fbdebc0194914c650471e9b55cdb053102ac
Blwan <forever27drunk@gmail.com>
2021-03-09 00:00:51 +0500

    w hardLesson

--- script/script.js
+++ script/script.js
@@ -1,33 +1,33 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
-    const countTimer = function (deadline) {
+    const countTimer = (deadline) => {
 
         let timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining(){
+        function getTimeRemaining() {
             let dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeReamaining = (dateStop - dateNow) / 1000,
                 seconds = Math.floor(timeReamaining % 60),
                 minutes = Math.floor((timeReamaining / 60) % 60),
                 hours = Math.floor(timeReamaining / 60 / 60);
-            
-            if(dateStop < dateNow){
-                return {timeReamaining: 0, hours: 0, seconds: 0, minutes: 0};
-            }else{
-                return {timeReamaining: timeReamaining, hours: hours, minutes: minutes, seconds: seconds};
+
+            if (dateStop < dateNow) {
+                return { timeReamaining: 0, hours: 0, seconds: 0, minutes: 0 };
+            } else {
+                return { timeReamaining: timeReamaining, hours: hours, minutes: minutes, seconds: seconds };
             }
         }
 
-        function updateClock(){
+        function updateClock() {
             let timer = getTimeRemaining();
-            
-            function plusZero(a){
-                if(a < 10){
-                    a =  '0' + a;
+
+            function plusZero(a) {
+                if (a < 10) {
+                    a = '0' + a;
                 }
                 return a;
             }
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
             timerMinutes.textContent = plusZero(timer.minutes);
             timerSeconds.textContent = plusZero(timer.seconds);
 
-            if(timer.timeReamaining > 0){
+            if (timer.timeReamaining > 0) {
                 setTimeout(updateClock);
             }
         }
@@ -66,31 +66,35 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const togglePopUp = () => {
         const popup = document.querySelector('.popup'),
-        popupBtn = document.querySelectorAll('.popup-btn'),
-        popupClose = document.querySelector('.popup-close'),
-        popupContent = document.querySelector('.popup-content');
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
 
         popupBtn.forEach((elem) => {
-            const popupAnimate = function() {
-                popup.style.display = 'block';
-                
-                let start = Date.now();
+            const popupAnimate = () => {
+                if (screen.width > 768) {
+                    popup.style.display = 'block';
 
-                function draw(timePassed) {
-                    popupContent.style.top = timePassed / 5 + 'px';
+                    let start = Date.now();
+
+                    const draw = (timePassed) => {
+                        popupContent.style.top = timePassed / 10 + 'px';
+                    };
+
+                    let timer = setInterval(function () {
+                        let timePassed = Date.now() - start;
+
+                        if (timePassed >= 2000) {
+                            clearInterval(timer);
+                            return;
+                        }
+
+                        draw(timePassed);
+
+                    }, 20);
+                } else {
+                    popup.style.display = 'block';
                 }
-
-                let timer = setInterval(function() {
-                let timePassed = Date.now() - start;
-
-                if (timePassed >= 2000) {
-                    clearInterval(timer); 
-                    return;
-                }
-
-                draw(timePassed);
-
-                }, 20);
             };
 
             elem.addEventListener('click', popupAnimate);
@@ -102,4 +106,22 @@ window.addEventListener('DOMContentLoaded', function() {
     };
 
     togglePopUp();
+
+    const scrollBtn = () => {
+        const anchors = document.querySelectorAll('a');
+
+        anchors.forEach((anchor) => {
+            anchor.addEventListener('click', (elem) => {
+                elem.preventDefault();
+
+                const blockID = anchor.getAttribute('href');
+
+                document.querySelector(blockID).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            });
+        });
+    };
+    scrollBtn();
 });
