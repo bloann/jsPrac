5f819a1456d12b11d1c110b7febe799a84e65fee
Blwan <forever27drunk@gmail.com>
2021-03-11 14:11:55 +0500

    ready w hard

--- script/script.js
+++ script/script.js
@@ -339,24 +339,86 @@ window.addEventListener('DOMContentLoaded', function () {
                 item.value = item.value.replace(/\-+/g, '-');
 
                 let allItems = item.value.match(/./g);
+                if (allItems) {
+                    for (let i = 0; i < allItems.length; i++) {
 
-                for (let i = 0; i < allItems.length; i++) {
-
-                    if ((i === 0 || i === allItems.length) &&
-                        (allItems[i] === ' ' || allItems[i] === '-')) {
+                        if ((i === 0 || i === allItems.length) &&
+                            (allItems[i] === ' ' || allItems[i] === '-')) {
                             allItems.splice(i, 1);
+                        }
+
                     }
-            
+
+                    item.value = allItems.join('');
+                    if (item.placeholder === 'Ваше имя') {
+                        item.value = item.value[0].toUpperCase() + item.value.slice(1);
+                    }
                 }
 
-                item.value = allItems.join('');
-                if(item.placeholder === 'Ваше имя'){
-                    item.value = item.value[0].toUpperCase() + item.value.slice(1);
-                }
             });
         });
     };
     inputCheck();
 
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
 
+        const animateValue = (obj, start, end, duration) => {//https://codepen.io/chriscoyier/pen/xxVBqEg
+            let startTimestamp = null;
+            const step = (timestamp) => {
+                if (!startTimestamp) {
+                    startTimestamp = timestamp; 
+                }
+                    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+                    obj.innerHTML = Math.floor(progress * (end - start) + start);
+
+                if (progress < 1) {
+                    window.requestAnimationFrame(step);
+                }
+            };  
+            window.requestAnimationFrame(step);
+        };
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value < 5 && calcDay.value) {
+                dayValue *= 2;
+            } else if (calcDay.value < 10 && calcDay.value) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = Math.floor(price * typeValue * squareValue * countValue * dayValue);
+            } else {
+                total = 0;
+            }
+
+            animateValue(totalValue, 0, total, 750);
+        };
+
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+    };
+    calc(100);
 });
